914c4fa71aec3c6c67e7a837dd4e25804ab6b8eb
ConejoMoncada <andresm1996@hotmail.com>
2018-08-17 16:22:00 -0600

    Corregir validaciones de seleccionar, agregar validaciones de mover y cambiar el tablero para reflejar el tipo de movida y cambiar el simbolo de las piezas cuando se necesite

--- MainExamen.cpp
+++ MainExamen.cpp
@@ -5,28 +5,127 @@ using namespace std;
 
 char** inicio();
 void print(char**);
-int espacios = 137;
-int v;//para las validaciones
-char ccol;//char columna
-int fi,ci,cd,fd;//filas y columnas iniciales y de destino
+void cambiar(char**,bool,int,int);
+
+
 main(){
+	Validaciones* val = new Validaciones();
+	int espacios = 137;
+	int v;//para las validaciones
+	char ccol;//char columna
+	int fi,ci,cd,fd;//filas y columnas iniciales y de destino
 	char** tablero = inicio();
 	bool fin = false;
 	bool turno = true;//true = +
-	print(tablero);
 	while(!fin){
+		print(tablero);
 		cout << "Turno de ";
 		if(turno)
 			cout << "+";
 		else 
 			cout << "#";
-		cout << endl;
-		
+		cout << endl <<"Seleccionar pieza"<< endl <<"Ingrese una columna (letras minúsculas) o ingrese 'z' para terminar: ";
+		cin >> ccol;
+		ci = val->convertir(ccol);
+		v = 2;//v=2 significa que la columna está bien, para que cuando se reinicie el do-wile revise la fila si la columna está bien
+		if(ccol != 'z'){
+			//validar seleccion
+			//v=1 -> todo está bien
+			do{
+				if(ci == 0 || v != 2){
+					cout << "Valor no válido" << endl <<"Ingrese una columna(letras minúsculas) o ingrese 'z' para terminar: ";
+					cin >> ccol;
+					ci = val->convertir(ccol);
+					v = 2;
+				}
+				else{
+					cout << "Ingrese una fila (números): ";
+					cin >> fi;
+					v = val->seleccionar(tablero,turno,fi,ci);
+					switch(v){
+						case -1: cout << "Posición fuera del tablero" << endl;
+							 break;
+						case 0: cout << "Posición no válida" << endl;
+					}
+				}
+			}while(v != 1);
+			do{
+				cout << "Ingrese la posición donde se quiere mover" << endl << "Columna (letras): ";
+				cin >> ccol;
+				cd = val->convertir(ccol);
+				cout << "Fila (números: ";
+				cin >> fd;
+				v = val->valMov(tablero,fi,ci,fd,cd);
+				switch(v){
+					case -1: cout << "Posición fuera del tablero" << endl;
+						 break;
+					case 0: cout << "Posición no válida" << endl;
+				}
+				switch(v){
+					case 1: if(turno)
+							tablero[fd][cd] = '+';
+						else
+							tablero[fd][cd] = '#';
+						break;
+					case 2: if(turno){
+							tablero[fd][cd] = '+';
+							tablero[fi][ci] = '.';
+						}
+						else{
+							tablero[fd][cd] = '#';
+							tablero[fi][ci] = '.';
+						}
+						break;
+				}
+			}while(v < 1);
+			switch(v){
+				case 1: if(turno)
+						tablero[fd][cd] = '+';
+					else
+						tablero[fd][cd] = '#';
+					break;
+				case 2: if(turno){
+						tablero[fd][cd] = '+';
+						tablero[fi][ci] = '.';
+					}
+					else{
+						tablero[fd][cd] = '#';
+						tablero[fi][ci] = '.';
+					}
+					break;
+			}
+			cambiar(tablero,turno,fd,cd);
+			turno = !turno;
+			if(turno)
+				tablero[0][0] = '+';
+			else
+				tablero[0][0] = '#';
+		}
+		if(espacios == 0 || ccol == 'z')
+			fin = true;
 	}
+	int cm = 0;//contador +
+	int cn = 0;//contador #
+	//revisar quien gana
+	for(int i = 1; i < 12; i++){
+		for (int j = 1; j < 12; j++){
+			if(tablero[i][j] == '+')
+				cm++;
+			else if(tablero[i][j] == '#')
+				cn++;
+		}
+	}
+	if(cn<cm)
+		cout << "Ganó +" << endl;
+	else if(cm<cn)
+		cout << "Ganó #" << endl;
+	else
+		cout << "Empate" << endl;
 	for (int i = 0; i < 12; i++){
 		delete[] tablero[i];
 	}
 	delete[] tablero;
+	delete val;
 }
 
 char** inicio(){
@@ -34,6 +133,7 @@ char** inicio(){
 	for(int i = 0; i < 12; i++){
 		t[i] = new char[12];
 	}
+	//llenar la matriz
 	for(int i = 1; i < 12; i++){
 		for (int j = 1; j < 12; j++){
 			t[i][j] = '.';
@@ -79,5 +179,108 @@ void print(char** t){
 			cout << t[i][j] << " ";
 		}
 		cout << endl;
+	}
+}
+
+void cambiar(char** tab, bool t, int f, int c){
+	bool ar = false,ab = false ,d = false,i = false,
+	     ar2 = false,ab2 = false,d2 = false,i2 = false;//son para revisar si hay espacio antes del borde
+	if(f > 1)
+		ar = true;
+	if(f < 11)
+		ab = true;
+	if(c > 1)
+		i = true;
+	if(c < 11)
+		d = true;
+	//turno de +
+	if(t){
+		//fila de arriba
+		if(ar){
+			//arriba
+			if(tab[f-1][c] == '#')
+				tab[f-1][c] = '+';
+			//arriba izquierda
+			if(i){
+				if(tab[f-1][c-1] == '#')
+					tab[f-1][c-1] = '+';
+			}
+			//arriba derecha
+			if(d){
+				if(tab[f-1][c+1] == '#')
+					tab[f-1][c+1] = '+';
+			}
+		}
+		//fila de abajo
+		if(ab){
+			//arriba
+			if(tab[f+1][c] == '#')
+				tab[f+1][c] = '+';
+			//arriba izquierda
+			if(i){
+				if(tab[f+1][c-1] == '#')
+					tab[f+1][c-1] = '+';
+			}
+			//arriba derecha
+			if(d){
+				if(tab[f+1][c+1] == '#')
+					tab[f+1][c+1] = '+';
+			}
+		}
+		//izquierda
+		if(i){
+			if(tab[f][c-1] == '#')
+				tab[f][c-1] = '+';
+		}
+		//derecha
+		if(d){
+			if(tab[f][c+1] == '#')
+				tab[f][c+1] = '+';
+		}
+	}
+	//turno de #
+	else{
+		//fila de arriba
+		if(ar){
+			//arriba
+			if(tab[f-1][c] == '+')
+				tab[f-1][c] = '#';
+			//arriba izquierda
+			if(i){
+				if(tab[f-1][c-1] == '+')
+					tab[f-1][c-1] = '#';
+			}
+			//arriba derecha
+			if(d){
+				if(tab[f-1][c+1] == '+')
+					tab[f-1][c+1] = '#';
+			}
+		}
+		//fila de abajo
+		if(ab){
+			//arriba
+			if(tab[f+1][c] == '+')
+				tab[f+1][c] = '#';
+			//arriba izquierda
+			if(i){
+				if(tab[f+1][c-1] == '+')
+					tab[f+1][c-1] = '#';
+			}
+			//arriba derecha
+			if(d){
+				if(tab[f+1][c+1] == '+')
+					tab[f+1][c+1] = '#';
+			}
+		}
+		//izquierda
+		if(i){
+			if(tab[f][c-1] == '+')
+				tab[f][c-1] = '#';
+		}
+		//derecha
+		if(d){
+			if(tab[f][c+1] == '+')
+				tab[f][c+1] = '#';
+		}
 	}
 }
